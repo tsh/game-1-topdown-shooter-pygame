5f7c52141aae239dc05370e86bccf08394411716
tsh <dr.tallin@gmail.com>
2017-08-26 21:44:13 +0300

    map placeholder

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ pygame.display.set_caption('Hello World')
 
 #Set up the colors
 BLACK = (0,0,0)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
 WHITE = (255,255,255)
 
 #Set up fonts
@@ -33,6 +30,9 @@ windowSurface.fill(Color("red"), r)
 
 #Draw the text onto the surface
 windowSurface.blit(text,textRect)
+
+map = Map()
+map.render(windowSurface)
 
 #Draw the window onto the screen
 pygame.display.update()
---
+++ map.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.locals import *
+
+
+class Map(object):
+    def __init__(self):
+        self.size = 100
+        self.map = [[1, 0], [0, 1]]
+
+    def render(self, surface):
+        for irow, row in enumerate(self.map):
+            for icol, tile in enumerate(row):
+                r = pygame.Rect(irow*self.size, icol*self.size, self.size, self.size)
+                color = "blue"
+                if tile == 0:
+                    color = "black"
+                elif tile == 1:
+                    color = "red"
+                surface.fill(Color(color), r)
