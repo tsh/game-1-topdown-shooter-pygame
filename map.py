94497122e3bf49bd1a4ce885d67f446a48baaac6
tsh <dr.tallin@gmail.com>
2017-09-17 23:07:47 +0300

    draw only visible tiles

--- map.py
+++ map.py
@@ -12,19 +12,19 @@ class Map(object):
         wrld_size = 1000
         wrld_srf = pygame.Surface((wrld_size, wrld_size))
         wrld_srf.fill((255, 255, 255))
+        # Draw map
         for irow, row in enumerate(self.map):
             for icol, tile in enumerate(row):
                 r = pygame.Rect(icol*self.size, irow*self.size, self.size, self.size)
-                color = "blue"
-                if tile == 0:
-                    color = "black"
-                elif tile == 1:
-                    color = "red"
-                elif tile == 2:
-                    color = "orange"
-                wrld_srf.fill(Color(color), r)
+                if camera.viewport.colliderect(r):
+                    color = "blue"
+                    if tile == 0:
+                        color = "black"
+                    elif tile == 1:
+                        color = "red"
+                    elif tile == 2:
+                        color = "orange"
+                    wrld_srf.fill(Color(color), r)
         x = camera.viewport.x if camera.viewport.x > 0 else 0
         y = camera.viewport.y if camera.viewport.y > 0 else 0
-
-        print ('camera map ', camera.viewport)
         window_surface.blit(wrld_srf, (-x, -y))
