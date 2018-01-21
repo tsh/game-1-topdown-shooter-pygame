afcd60191ee5c576aff2d2e40f82ca53ff7c5055
tsh <dr.tallin@gmail.com>
2018-01-21 14:03:15 +0200

    create Game class

--- main.py
+++ main.py
@@ -31,77 +31,80 @@ class GameObjectManager(object):
                 prj.remove = True
 
 
-#Set up pygame
-pygame.init()
+class Game(object):
+    def __init__(self):
+        pygame.init()
+        #Set up the window
+        self.windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+        pygame.display.set_caption('Hello World')
+        #Set up fonts
+        basicFont = pygame.font.SysFont(None, 48)
+        self.text = basicFont.render('HELLO WORLD', True, BLACK)
+        self.textRect = self.text.get_rect()
+        self.textRect.centerx = self.windowSurface.get_rect().centerx
+        self.textRect.centery = self.windowSurface.get_rect().centery
 
-#Set up the window
-windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-pygame.display.set_caption('Hello World')
+        self.map = Map()
+        self.chr = Character(HALF_WIDTH, HALF_HEIGHT)
+        self.enm = Enemy()
+        self.gom = GameObjectManager(WIDTH, HEIGHT)
+        self.camera = Camera(WIDTH, HEIGHT)
 
-#Set up fonts
-basicFont = pygame.font.SysFont(None, 48)
-text = basicFont.render('HELLO WORLD', True, BLACK)
-textRect = text.get_rect()
-textRect.centerx = windowSurface.get_rect().centerx
-textRect.centery = windowSurface.get_rect().centery
+    def game_loop(self):
+        clock = pygame.time.Clock()
+        #Run the game loop
+        while True:
+            clock.tick(60)
 
-map = Map()
-chr = Character(HALF_WIDTH, HALF_HEIGHT)
-enm = Enemy()
-gom = GameObjectManager(WIDTH, HEIGHT)
-camera = Camera(WIDTH, HEIGHT)
+            key_pressed = pygame.key.get_pressed()
+            if key_pressed[pygame.K_w]:
+                self.chr.move(self.chr.FORWARD)
+                self.camera.viewport.centery -= 10
+                print('Forward')
+            elif key_pressed[pygame.K_s]:
+                self.chr.move(self.chr.BACKWARD)
+                self.camera.viewport.centery += 10
+                print('Backward')
+            elif key_pressed[pygame.K_a]:
+                self.chr.move(self.chr.LEFT)
+                self.camera.viewport.centerx -= 10
+                print('Stroll Left')
+            elif key_pressed[pygame.K_d]:
+                self.chr.move(self.chr.RIGHT)
+                self.camera.viewport.centerx += 10
+                print('Stroll Right')
 
-clock = pygame.time.Clock()
-#Run the game loop
-while True:
-    clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.chr.shoot(self.windowSurface, direction=UP)
+                        print('Up')
+                    elif event.key == pygame.K_DOWN:
+                        self.chr.shoot(self.windowSurface, direction=DOWN)
+                        print('Down')
+                    elif event.key == pygame.K_LEFT:
+                        self.chr.shoot(self.windowSurface, direction=LEFT)
+                        print('Left')
+                    elif event.key == pygame.K_RIGHT:
+                        self.chr.shoot(self.windowSurface, direction=RIGHT)
+                        print('Right')
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    self.chr.shoot(self.windowSurface, target=Vec2d(pos))
+                    print(pos)
 
-    key_pressed = pygame.key.get_pressed()
-    if key_pressed[pygame.K_w]:
-        chr.move(chr.FORWARD)
-        camera.viewport.centery -= 10
-        print('Forward')
-    elif key_pressed[pygame.K_s]:
-        chr.move(chr.BACKWARD)
-        camera.viewport.centery += 10
-        print('Backward')
-    elif key_pressed[pygame.K_a]:
-        chr.move(chr.LEFT)
-        camera.viewport.centerx -= 10
-        print('Stroll Left')
-    elif key_pressed[pygame.K_d]:
-        chr.move(chr.RIGHT)
-        camera.viewport.centerx += 10
-        print('Stroll Right')
+            self.map.render(self.windowSurface, self.camera)
+            self.windowSurface.blit(self.text ,self.textRect)
+            self.chr.render(self.windowSurface)
+            self.enm.render(self.windowSurface)
+            self.gom.check_boundry()
+            Projectile.update(self.windowSurface)
+            pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                chr.shoot(windowSurface, direction=UP)
-                print('Up')
-            elif event.key == pygame.K_DOWN:
-                chr.shoot(windowSurface, direction=DOWN)
-                print('Down')
-            elif event.key == pygame.K_LEFT:
-                chr.shoot(windowSurface, direction=LEFT)
-                print('Left')
-            elif event.key == pygame.K_RIGHT:
-                chr.shoot(windowSurface, direction=RIGHT)
-                print('Right')
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            chr.shoot(windowSurface, target=Vec2d(pos))
-            print(pos)
+            self.camera.move(chr)
 
-    map.render(windowSurface, camera)
-    windowSurface.blit(text,textRect)
-    chr.render(windowSurface)
-    enm.render(windowSurface)
-    gom.check_boundry()
-    Projectile.update(windowSurface)
-    pygame.display.update()
-
-    camera.move(chr)
+game = Game()
+game.game_loop()
 sys.exit()
